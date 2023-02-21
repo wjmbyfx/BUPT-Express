774a3722b956302bdf63caddeacb081495038456
miul <wjm5490@gmail.com>
2023-02-21 18:44:10 +0800

    img updated

--- code/cloud/getImage/index.js
+++ code/cloud/getImage/index.js
@@ -5,10 +5,10 @@ cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-    const ill01='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill01.png'
-    const ill02='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill02.png'
-    const ill03='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill03.jpeg'
-    const ill04='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill04.jpeg'
+    const ill01='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill01.jpg'
+    const ill02='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill02.jpg'
+    const ill03='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill03.png'
+    const ill04='cloud://buptexpress-5goetp5o571a721f.6275-buptexpress-5goetp5o571a721f-1316844506/ill04.png'
     const list=[ill01,ill02,ill03,ill04]
     function getRandomInt(max){
         return Math.floor(Math.random()*max)
