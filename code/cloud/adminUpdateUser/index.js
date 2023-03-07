4ab2ef63e72929fa8e77b62e7b5b64dce33f93e5
miul <wjm5490@gmail.com>
2023-03-07 13:43:37 +0800

    admin api added

---
+++ code/cloud/adminUpdateUser/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    const wxContext = cloud.getWXContext()
+    const openid=event.openid
+    const credit=event.credit
+    cloud.database().collection('user').where({openid:openid}).update({data:{
+        credit:credit
+    }})
+    return {
+        event,
+        openid: wxContext.OPENID,
+        appid: wxContext.APPID,
+        unionid: wxContext.UNIONID,
+    }
+}
