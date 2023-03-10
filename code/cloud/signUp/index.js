3f6a372566b5888746c06c9abfb7709342086c4e
miul <wjm5490@gmail.com>
2023-03-10 10:22:56 +0800

    user signup api updated

--- code/cloud/signUp/index.js
+++ code/cloud/signUp/index.js
@@ -14,12 +14,12 @@ exports.main = async (event, context) => {
         if(event.isMember==true){
             await cloud.database().collection('user')
             .where({openid:wxContext.OPENID})
-            .update({data:{username:event.username,location:{building:event.building,floor:event.floor},contact:event.contact,openid:wxContext.OPENID,credit:100}})
+            .update({data:{username:event.username,location:{building:event.building,floor:event.floor},contact:event.contact,openid:wxContext.OPENID,credit:100,status:1}})
             type='update'
         }
         if(event.isMember==false){
             await cloud.database().collection('user')
-            .add({data:{username:event.username,location:{building:event.building,floor:event.floor},contact:event.contact,openid:wxContext.OPENID,credit:100}})
+            .add({data:{username:event.username,location:{building:event.building,floor:event.floor},contact:event.contact,openid:wxContext.OPENID,credit:100,status:1}})
             type='signUp'
         }
         return {msg:'success',type:type}
